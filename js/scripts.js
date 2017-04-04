a560b0f0f5f9e90b6c9f3dd104654239897cc530
Katie Borel and Starlynne Start <starlynne@>
2017-04-04 13:56:28 -0700

    Gather user input and parseInt number values

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,40 @@
 //Buisness logic
+function Account (firstName, initDeposit) {
+  this.firstName = firstName;
+  this.initDeposit = initDeposit;
+}
+
+Account.prototype.depositAdd = function(){
+  initDeposit + depositInput
+}
+
 
 //User Interface
 $(function() {
-  
+  $("#account-setup").submit(function(event) {
+    event.preventDefault();
+
+    var nameInput = $("#first-name").val();
+    var initDeposit = parseInt($("#init-deposit").val());
+
+    newAccount = new Account(nameInput, initDeposit);
+
+    console.log(newAccount);
+  });
+
+  $("#operations").submit(function(event) {
+    event.preventDefault();
+
+    var depositInput = parseInt($("#deposit").val());
+    var withdrawalInput = parseInt($("#withdrawal").val());
+
+
+
+    console.log(depositInput, withdrawalInput);
+  });
+
+
+
+
+
 });
